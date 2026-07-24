2373f1c75017a54cb9e431e663ab528cfa40d8ac
axelomcava-hash <axel.omcava@gmail.com>
2026-07-24 16:55:45 -0600

    Fix: Allow server to listen on all network interfaces (0.0.0.0) for mobile access

--- server.js
+++ server.js
@@ -158,6 +158,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor iniciado en http://127.0.0.1:${port}`);
 });
